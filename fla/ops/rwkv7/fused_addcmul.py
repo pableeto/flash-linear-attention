3b2d987b78ea69625bc51d626b98e9ad36fc3d29
Xiao Li <xili11@microsoft.com>
2025-06-19 02:48:03 +0000

    fix rwkv7

--- fla/ops/rwkv7/fused_addcmul.py
+++ fla/ops/rwkv7/fused_addcmul.py
@@ -7,7 +7,8 @@ import torch
 import triton
 import triton.language as tl
 
-from fla.utils import check_pytorch_version, input_guard, use_cuda_graph
+from fla.utils import check_pytorch_version, input_guard, use_cuda_graph, is_amd
+NUM_WARPS_AUTOTUNE = [2, 4, 8, 16] if is_amd else [2, 4, 8, 16, 32]
 
 logger = logging.getLogger(__name__)
 
@@ -18,7 +19,7 @@ if not check_pytorch_version('2.4'):
 @triton.autotune(
     configs=[
         triton.Config({}, num_warps=num_warps)
-        for num_warps in [1, 2, 4, 8, 16, 32]
+        for num_warps in NUM_WARPS_AUTOTUNE
     ],
     key=['D'],
     use_cuda_graph=use_cuda_graph,
@@ -77,7 +78,7 @@ def fused_addcmul_fwd_kernel(
 @triton.autotune(
     configs=[
         triton.Config({}, num_warps=num_warps)
-        for num_warps in [1, 2, 4, 8, 16, 32]
+        for num_warps in NUM_WARPS_AUTOTUNE
     ],
     key=['D'],
     use_cuda_graph=use_cuda_graph,
